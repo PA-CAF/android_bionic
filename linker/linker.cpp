3fd5b5e664cc9fcfa7ca14fc81db77802c8b754e
Christopher N. Hesse <raymanfx@gmail.com>
2016-08-28 15:29:08 +0200

    linker: Allow text relocations
    Change-Id: I672b5df08ec8d7e623d83da1f402013e555eca2a

--- linker/linker.cpp
+++ linker/linker.cpp
@@ -4070,8 +4070,12 @@ bool soinfo::link_image(const soinfo_list_t& global_group, const soinfo_list_t& 
     if (get_application_target_sdk_version() > 22) {
 #endif
       PRINT("%s: has text relocations", get_realpath());
+#if defined(ALLOW_TEXT_RELOCATIONS)
+      DL_WARN("%s: has text relocations", get_realpath());
+#else
       DL_ERR("%s: has text relocations", get_realpath());
       return false;
+#endif
     }
 #endif
     // Make segments writable to allow text relocations to work properly. We will later call
